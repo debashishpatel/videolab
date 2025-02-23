ff15008feed24909b32c0c63c513f10dae88c61c
debashishpatel <debashishpatel10@gmail.com>
2025-02-24 02:02:13 +0530

    feb 2025 25

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from "cors"
+import healthCheckRouter from './src/routes/healthcheck.routes.js'
 
 const app = express()
 
@@ -14,5 +15,8 @@ app.use(cors(
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({limit: '50mb', extended: true}))
 app.use(express.static('public'))
+
+//routes
+app.use("/api/v1/healthcheck", healthCheckRouter)
 
 export { app }
